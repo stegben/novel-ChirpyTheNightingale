156ac0974943d7a4b87ffb60ff3b25d6f4d48eda
cph <stegben.benjamin@gmail.com>
2016-09-17 02:42:08 +0800

    modify webpack dev, add images

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
+/* eslint-disable */
 import React from 'react';
 import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 
-import Title from './components/Title';
+import Novel from './components/Novel';
 
-const App = () => (
-  <Title />
-);
+const rootElement = document.getElementById('root');
 
-render(<App />, document.getElementById('root'));
+let app;
+
+if (module.hot) {
+  app = (
+    <AppContainer>
+      <Novel />
+    </AppContainer>
+  );
+
+  module.hot.accept('./components/Novel', () => {
+    const NewRoot = require('./components/Novel').default;
+
+    render(
+      <AppContainer>
+        <NewRoot />
+      </AppContainer>,
+      rootElement
+    );
+  });
+} else {
+  app = <Novel />;
+}
+
+render(app, rootElement);
