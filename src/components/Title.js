f41424a36728e9be0fabcf04d49283b772946441
cph <stegben.benjamin@gmail.com>
2016-09-17 15:11:00 +0800

    add background video

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,15 @@ import React from 'react';
 import style from './Title.module.css';
 
 const Title = () => (
-  <div className={style.title}>Chirpy</div>
+  <div>
+    <video
+      className={style.titleBackgroundVid}
+      autoPlay
+      loop
+    >
+      <source src="https://s3-ap-southeast-1.amazonaws.com/novel-videos/aurora-medium.mp4" type="video/mp4" />
+    </video>
+  </div>
 );
 
 export default Title;
