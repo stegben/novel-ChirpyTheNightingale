794f90789cfc2208e5037e65f8eb882b8780918b
cph <stegben.benjamin@gmail.com>
2016-09-17 15:48:28 +0800

    done title

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,12 +5,19 @@ import style from './Title.module.css';
 const Title = () => (
   <div>
     <video
-      className={style.titleBackgroundVid}
+      className={style.backgroundVid}
       autoPlay
+      muted
       loop
     >
-      <source src="https://s3-ap-southeast-1.amazonaws.com/novel-videos/aurora-medium.mp4" type="video/mp4" />
+      <source src="https://s3-ap-southeast-1.amazonaws.com/novel-videos/aurora.mp4" type="video/mp4" />
     </video>
+    <div className={style.title}>
+      靈鳥小啾
+    </div>
+    <div className={style.authors}>
+      文：凝人  圖：Shaii
+    </div>
   </div>
 );
 
