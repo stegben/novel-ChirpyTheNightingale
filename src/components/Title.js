2c5d0904010babde7ca1f4d5628c5a1ebfb41b13
cph <stegben.benjamin@gmail.com>
2016-09-17 19:30:41 +0800

    wrap title background video

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,14 +4,16 @@ import style from './Title.module.css';
 
 const Title = () => (
   <div>
-    <video
-      className={style.backgroundVid}
-      autoPlay
-      muted
-      loop
-    >
-      <source src="https://s3-ap-southeast-1.amazonaws.com/novel-videos/aurora.mp4" type="video/mp4" />
-    </video>
+    <div className={style.videoContainer}>
+      <video
+        className={style.backgroundVid}
+        autoPlay
+        muted
+        loop
+      >
+        <source src="https://s3-ap-southeast-1.amazonaws.com/novel-videos/aurora.mp4" type="video/mp4" />
+      </video>
+    </div>
     <div className={style.title}>
       靈鳥小啾
     </div>
